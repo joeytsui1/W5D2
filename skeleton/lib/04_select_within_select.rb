bf12989ac4e54490ee9e0de7443745342df14685
Joey Tsui <joeytsui131@gmail.com>
2022-12-06 16:21:16 -0500

    part 4

--- skeleton/lib/04_select_within_select.rb
+++ skeleton/lib/04_select_within_select.rb
@@ -35,6 +35,18 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    countries
+  WHERE
+   population > (SELECT
+      population
+    FROM
+      countries
+    WHERE
+      name = 'Russia'
+    )
   SQL
 end
 
@@ -42,6 +54,17 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+  SELECT
+    name
+  FROM 
+    countries
+  WHERE
+    continent = 'Europe' AND (gdp/population) > (SELECT
+      (gdp/population) AS capita_gdp
+    FROM
+      countries
+    WHERE
+      name = 'United Kingdom')
   SQL
 end
 
@@ -49,6 +72,7 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+  
   SQL
 end
 
